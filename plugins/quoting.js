d71f4ac9e335837512a8fd028d3538bb162601c6
Nikki <nikki@nikkii.us>
2014-08-20 14:41:42 -0400

    Better quoting without hooking updating all the time

--- plugins/quoting.js
+++ plugins/quoting.js
@@ -1,15 +1,7 @@
 define(function() {
 	var ShoutQuotingPlugin = function(mod) {
-		mod.on('update_shouts', function() {
-			$('div', InfernoShoutbox.shoutframe).each(function (index) {
-				if (this.ondblclick != null) {
-					return;
-				}
-
-				$(this).dblclick(function () {
-					InfernoShoutbox.editor.value = PHP.trim($(this).text());
-				});
-			});
+		$('#shoutbox_frame > div.smallfont').on('dblclick', function() {
+			InfernoShoutbox.editor.value = PHP.trim($(this).text());
 		});
 	};
 
