9f45ec003be871fc1f79264eb2936638baba76d8
nikkiii <nikkiii@users.noreply.github.com>
2016-05-21 21:26:52 -0400

    Update the loader to use SSL
    Update the loader to use SSL

--- infernoshoutmod.user.js
+++ infernoshoutmod.user.js
@@ -3,12 +3,12 @@
 // @namespace	  http://rune-server.org/
 // @include     *.rune-server.org/forum.php
 // @include     *.rune-server.org/infernoshout.php?do=detach
-// @version		1
+// @version		1.1
 // ==/UserScript==
 
 var scriptNode = document.createElement("script");
 scriptNode.setAttribute("type", "text/javascript");
-scriptNode.setAttribute("src", "http://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js");
-scriptNode.setAttribute("data-main", "http://nikkii.us/sbmod/loader.js");
+scriptNode.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js");
+scriptNode.setAttribute("data-main", "https://nikkii.us/sbmod/loader.js");
 scriptNode.setAttribute("async", true);
 document.getElementsByTagName("head")[0].appendChild(scriptNode);
